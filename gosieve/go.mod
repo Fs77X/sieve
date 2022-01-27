906eb7ae1bf5723f53a2bcbfe1ff5534a7400589
fs77x <farhansaeed1999@gmail.com>
2022-01-27 04:46:18 -0800

    Porting gdprbench to sieve data

--- gosieve/go.mod
+++ gosieve/go.mod
@@ -3,6 +3,7 @@ module gosieve
 go 1.17
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
--- gosieve/main.go
+++ gosieve/main.go
@@ -1,8 +1,5 @@
 package main
-	// "math/rand"
-	// 
-	// "strconv"
-	// "time"
+
 import (
 	"fmt"
 	"encoding/json"
@@ -11,7 +8,10 @@ import (
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"database/sql"
-	"github.com/satori/go.uuid"
+	"github.com/google/uuid"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 // Currently hosting db server locally, db credentials below
@@ -37,8 +37,8 @@ type mData struct {
 	CAT       string `json:"cat"`
 }
 
-counter := 1000
-
+var counter int = 1000
+var counter1 int = 10000
 func main(){
 	router := mux.NewRouter()
 	
@@ -50,14 +50,55 @@ func main(){
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func add_policy(mData data, db *sql.DB) string, int {
+func add_policy(data mData, db *sql.DB) (string, int) {
 	// TODO: add uuid gen, getting data in
+	uniqueid := uuid.New().String()
+	q := strconv.Itoa(rand.Intn(99) + 1)
+	enf_action := "allow"
+	currTime := time.Now() //.String()
+	_, err := db.Exec(`INSERT INTO user_policy(policy_id, id, querier,
+		 purpose, ttl, adm, origin, objection, sharing,
+		 enforcement_action, inserted_at)
+		 VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`, counter, uniqueid, q, data.Purpose, data.TTL, data.Adm, data.Origin, data.Objection, data.Sharing, enf_action, currTime)
+	checkErr(err)
+	counter = counter + 1
+	return uniqueid, 0
 
 }
 
+func getuIdData(mID string,db *sql.DB) (string, string) {
+	rows, err := db.Query("SELECT userid, dataVal FROM gdpr_data WHERE id = $1;", mID)
+	checkErr(err)
+	var resUserId string
+	var resData string
+	for rows.Next() {
+		var id string
+		var dataVal string
+		err1 := rows.Scan(&id, &dataVal)
+		checkErr(err1)
+		resUserId = id
+		resData = dataVal
+	}
+	return resUserId, resData
+}
+
 //use same uuid from add_policy
-func add_objcond(){
+func add_objcond(mID string, uID string, db *sql.DB) int {
 	// TODO: getting data in with corresponding uuidgen
+	attrib0 := "userid"
+	attrib1 := "dataVal"
+	attribute_type := "STRING"
+	operator := "="
+	compVal0, compVal1 := getuIdData(mID, db)
+	_, err := db.Exec(`INSERT INTO user_policy_object_condition(id, policy_id, attribute, attribute_type, operator, comp_value) VALUES(
+		$1, $2, $3, $4, $5, $6);`, counter1, uID, attrib0, attribute_type, operator, compVal0)
+	checkErr(err)
+	counter1 = counter1 + 1
+	_, err1 := db.Exec(`INSERT INTO user_policy_object_condition(id, policy_id, attribute, attribute_type, operator, comp_value) VALUES(
+		$1, $2, $3, $4, $5, $6);`, counter1, uID, attrib1, attribute_type, operator, compVal1)
+	counter1 = counter1 + 1
+	checkErr(err1)
+	return 0
 }
 
 func addMetaData(w http.ResponseWriter, r *http.Request){
@@ -88,8 +129,26 @@ func addMetaData(w http.ResponseWriter, r *http.Request){
 		db.Close()
 		return
 	}
-
-	status := 200
+	uID, res := add_policy(data, db)
+	if res != 0 {
+		status := 500
+		w.WriteHeader(status)
+		response = JsonResponse{Type: "error", Message: "Something wrong!"}
+		json.NewEncoder(w).Encode(response)
+		db.Close()
+		return
+	}
+	res = add_objcond(mID, uID, db)
+	if res != 0 {
+		status := 500
+		w.WriteHeader(status)
+		response = JsonResponse{Type: "error", Message: "Something wrong!"}
+		json.NewEncoder(w).Encode(response)
+		db.Close()
+		return
+	}
+	fmt.Println("gucci")
+	status := 201
 	w.WriteHeader(status)
 	response = JsonResponse{Type: "Success", Message: "Metadata entered"}
 	json.NewEncoder(w).Encode(response)
@@ -142,7 +201,8 @@ func createEntry(w http.ResponseWriter, r *http.Request) {
 	_, err := db.Exec("INSERT INTO gdpr_data(id, dataVal, userid) VALUES($1, $2, $3);", entryID, entryGpa, entryName)
 	db.Close()
 	checkErr(err)
-	status := 200
+	status := 201
+	fmt.Println("gucci")
 	response := JsonResponse{Type: "success", Message: "The entry has been inserted successfully!"}
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(response)
