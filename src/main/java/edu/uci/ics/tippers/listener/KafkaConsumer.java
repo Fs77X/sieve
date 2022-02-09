3122e4721b89e2d44e8336ffda4b7cffcccf3029
fs77x <farhansaeed1999@gmail.com>
2022-02-08 19:12:15 -0800

    got kafka data to work in java

--- src/main/java/edu/uci/ics/tippers/listener/KafkaConsumer.java
+++ src/main/java/edu/uci/ics/tippers/listener/KafkaConsumer.java
@@ -1,16 +1,19 @@
 package edu.uci.ics.tippers.listener;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import edu.uci.ics.tippers.model.middleware.mget_obj;
+
 @Service
 public class KafkaConsumer {
-   @KafkaListener(topics = "cloud", groupId = "group_id")
-   public void consume(String message) {
-       System.out.println("Consume message " + message);
-   } 
-   @KafkaListener(topics = "mget_obj", groupId = "group_json", containerFactory = "mget_objKakfaListenerFactory")
-   public void consumemget_obj(mget_obj qm) {
-       System.out.println("Consume json " + qm);
-   } 
+    @KafkaListener(topics = "cloud", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consume message " + message);
+    }
+
+    @KafkaListener(topics = "mget_obj", groupId = "group_json", containerFactory = "mget_objKakfaListenerFactory")
+    public void consumemget_obj(mget_obj qm) {
+        System.out.println("Consume json " + qm);
+    }
 }
--- src/main/java/edu/uci/ics/tippers/model/middleware/mget_obj.java
+++ src/main/java/edu/uci/ics/tippers/model/middleware/mget_obj.java
@@ -1,40 +1,36 @@
 package edu.uci.ics.tippers.model.middleware;
 
-import org.apache.tomcat.jni.Time;
+// import org.apache.tomcat.jni.Time;
 
 public class mget_obj {
-    private String[] id;
-    private PropInfo[] property;
+    private String id;
+    private String prop;
+    private String info;
     public mget_obj() {
 
     }
-    public mget_obj(String[] id, PropInfo[] property) {
+    public mget_obj(String id, String prop, String info) {
         this.id = id;
-        this.property = property;
+        this.prop = prop;
+        this.info = info;
     }
-    public String[] getId() {
+    public String getId() {
         return this.id;
     }
 
-    public PropInfo[] getProp() {
-        return this.property;
+    public String getProp() {
+        return this.prop;
+    }
+
+    public String getInfo() {
+        return this.info;
     }
 
     @Override public String toString() {
         StringBuilder sb = new StringBuilder("mget_obj{");
-        sb.append("id = [");
-        for(int i = 0; i < this.id.length; i++) {
-            sb.append("id: ").append(this.id[i]);
-            if (i != this.id.length-1) sb.append(", ");
-        }
-        sb.append("],\nproperty=[");
-        for(int i = 0; i < this.property.length; i++) {
-            sb.append("{prop: ").append(this.property[i].getProp()).append(", ");
-            sb.append("info: ").append(this.property[i].getInfo()).append("}");
-            if (i != this.property.length-1) sb.append(",");
-            sb.append("\n");
-        }
-        sb.append("]\n}");
+        sb.append("id: ").append(this.id).append(", ");
+        sb.append("prop: ").append(this.prop).append(", ");
+        sb.append("info: ").append(this.info).append("}");
         return sb.toString();
     }
 }
