790167917c0af60d2320b4557aa0a1fbdf5deb7c
farquad <farquad@pop-os.localdomain>
2022-05-01 15:16:45 -0700

    ttl daemon

---
+++ gosieve/baseTTL.go
@@ -0,0 +1,295 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"time"
+	"strconv"
+	// "io/ioutil"
+	"encoding/json"
+
+	_ "github.com/lib/pq"
+)
+
+// import (
+
+// 	"math/rand"
+// 	"strconv"
+
+// )
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "admin"
+	DB_NAME     = "the_encdb"
+)
+
+// type struct ttlRet {
+// 	Id string `json:"id"`
+// 	TTL int64 `json:"ttl"`
+// }
+
+// get id/ttls
+// compare with time
+// add id to different list for deletions
+// delete it
+
+type md struct {
+	Id  string `json:"id"`
+	TTL int64  `json:"ttl"`
+}
+
+type mde struct {
+	Id  string `json:"id"`
+	TTL string  `json:"ttl"`
+}
+
+type listResp struct {
+	ListMD []md `json:"listMd"`
+}
+
+func MDColUT(colname string, newMD *md) interface{} {
+	switch colname {
+	case "id":
+		return &newMD.Id
+	case "ttl":
+		return &newMD.TTL
+	default:
+		panic("unknown column " + colname)
+	}
+}
+
+func MDColUTe(colname string, newMD *mde) interface{} {
+	switch colname {
+	case "id":
+		return &newMD.Id
+	case "ttl":
+		return &newMD.TTL
+	default:
+		panic("unknown column " + colname)
+	}
+}
+
+func getTTLID(db *sql.DB) []md {
+	rows, err := db.Query("SELECT id, ttl from user_policy;")
+	checkErr(err)
+	var res []md
+	var columns []string
+	columns, err1 := rows.Columns()
+	checkErr(err1)
+	colNum := len(columns)
+	for rows.Next() {
+		var newMd md
+		cols := make([]interface{}, colNum)
+		for i := 0; i < colNum; i++ {
+			cols[i] = MDColUT(columns[i], &newMd)
+		}
+
+		err2 := rows.Scan(cols...)
+		checkErr(err2)
+		res = append(res, newMd)
+	}
+	return res
+}
+
+func getTTLIDEnc(db *sql.DB) []mde {
+	rows, err := db.Query("SELECT id, PGP_SYM_DECRYPT(ttl::bytea, " + "'key'" + ") as ttl from usertable")
+	checkErr(err)
+	var res []mde
+	var columns []string
+	columns, err1 := rows.Columns()
+	checkErr(err1)
+	colNum := len(columns)
+	for rows.Next() {
+		var newMd mde
+		cols := make([]interface{}, colNum)
+		for i := 0; i < colNum; i++ {
+			cols[i] = MDColUTe(columns[i], &newMd)
+		}
+
+		err2 := rows.Scan(cols...)
+		checkErr(err2)
+		res = append(res, newMd)
+	}
+	return res
+}
+
+func delTTL(db *sql.DB, listIDTTL []md, vac bool) {
+	currTime := time.Now().Unix()
+	for _, mdObj := range listIDTTL {
+		if mdObj.TTL < currTime {
+			_, err := db.Exec("DELETE FROM usertable where id = $1", mdObj.Id)
+			checkErr(err)
+			if vac {
+				_, err1 := db.Exec("VACUUM FULL usertable")
+				checkErr(err1)
+			}
+		}
+	}
+	fmt.Println("DELETE")
+}
+
+func delTTLenc(db *sql.DB, listIDTTL []mde, vac bool) {
+	currTime := time.Now().Unix()
+	for _, mdObj := range listIDTTL {
+		currObjTTL, err0 := strconv.ParseInt(mdObj.TTL, 10, 64)
+		checkErr(err0)
+		if currObjTTL < currTime {
+			_, err := db.Exec("DELETE FROM usertable where id = $1", mdObj.Id)
+			checkErr(err)
+			if vac {
+				_, err1 := db.Exec("VACUUM FULL usertable")
+				checkErr(err1)
+			}
+		}
+	}
+	fmt.Println("DELETE")
+}
+
+func delTombstone(db *sql.DB, listIDTTL []md) {
+	currTime := time.Now().Unix()
+	for _, mdObj := range listIDTTL {
+		// fmt.Println(mdObj.TTL)
+		if mdObj.TTL < currTime {
+			fmt.Println("UPDATE usertable set tomb = 1 where id = " + mdObj.Id)
+			_, err := db.Exec("UPDATE usertable set tomb = 1 where id = $1", mdObj.Id)
+			checkErr(err)
+		}
+	}
+	fmt.Println("DELETETOMB")
+}
+
+func delSeppy(db *sql.DB, listIDTTL []md) {
+	currTime := time.Now().Unix()
+	for _, mdObj := range listIDTTL {
+		// fmt.Println(mdObj.TTL)
+		if mdObj.TTL < currTime {
+			_, err := db.Exec("DELETE FROM mall_observation where id = $1", mdObj.Id)
+			checkErr(err)
+			_, err1 := db.Exec("DELETE FROM user_policy where id = $1", mdObj.Id)
+			checkErr(err1)
+
+		}
+	}
+	fmt.Println("DELSEPPY")
+
+}
+
+func delLSMeta(key string) {
+	url := "http://localhost:8000/mdelete_UserMetaobj/" + key
+	method := "DELETE"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("NO BUENO")
+	}
+}
+
+func delLSData(key string) {
+	url := "http://localhost:8000/mdelete_obj/" + key
+	method := "DELETE"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("NO BUENO")
+	}
+}
+
+func gdprLSTTL() {
+	url := "http://localhost:8080/getTTL"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	var data []md
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		checkErr(err)
+	}
+	// body, err := ioutil.ReadAll(res.Body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	currTime := time.Now().Unix()
+	var delIds []string
+	for i := 0; i < len(data); i++ {
+		if data[i].TTL < currTime {
+			delIds = append(delIds, data[i].Id)
+		}
+	}
+
+	for i := 0; i < len(delIds); i++ {
+		delLSData(delIds[i])
+		delLSMeta(delIds[i])
+	}
+
+}
+
+// basically same step with tombstoning
+func main() {
+	tick := time.Tick(2 * time.Second)
+	db := setupDB()
+	for range tick {
+		fmt.Println("Tick")
+		// listIDTTL := getTTLID(db)
+		listIDTTL := getTTLIDEnc(db)
+		// gdprLSTTL()
+		// delTombstone(db, listIDTTL)
+		delTTLenc(db, listIDTTL, true)
+		// delSeppy(db, listIDTTL)
+		// time.Sleep(5 * time.Second)
+	}
+}
+
+func setupDB() *sql.DB {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+
+	checkErr(err)
+
+	return db
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
---
+++ gosieve/refreshttl.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+// Currently hosting db server locally, db credentials below
+const (
+	DB_USER = "postgres"
+	DB_PASSWORD = "admin"
+	DB_NAME = "the_db"
+)
+
+type mData struct {
+	TTL       int64  `json:"TTL"`
+	Purpose   string `json:"purpose"`
+	Origin    string `json:"origin"`
+	Objection string `json:"objection"`
+	Sharing   string `json:"sharing"`
+}
+
+var counter int = 10000
+var ENFORCEMENT_ACTION string = "allow"
+
+func generateMData() mData {
+	var newMData mData
+	newMData.TTL = time.Now().Unix() + int64(rand.Intn(97000)+3000)
+	newMData.Purpose = "purpose" + strconv.Itoa(rand.Intn(99)+1)
+	newMData.Origin = "src" + strconv.Itoa(rand.Intn(99)+1)
+	newMData.Objection = "obj" + strconv.Itoa(rand.Intn(99)+1)
+	newMData.Sharing = "shr" + strconv.Itoa(rand.Intn(99)+1)
+	return newMData
+}
+
+func refreshTTL(listOfIds []string, db *sql.DB) {
+	for i, id := range listOfIds {
+		fmt.Println("iter: " + strconv.Itoa(i))
+		ttl := time.Now().Unix() + int64(rand.Intn(4000)+30)
+		_, err := db.Exec(`UPDATE usertable SET ttl = $1 WHERE id = $2`, ttl, id)
+		checkErr(err)
+	}
+}
+
+func insertPolicy(listOfIds []string, db *sql.DB) {
+	for _, id := range listOfIds {
+		gpdrMeta := generateMData()
+		querier := rand.Intn(39) + 1
+		inserted_at := time.Now()
+		_, err := db.Exec(`INSERT INTO user_policy(policy_id, id, querier,
+			purpose, ttl, origin, objection, sharing,
+			enforcement_action, inserted_at)
+			VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`, counter, id, querier, gpdrMeta.Purpose, gpdrMeta.TTL, gpdrMeta.Origin, gpdrMeta.Objection, gpdrMeta.Sharing, ENFORCEMENT_ACTION, inserted_at)
+		checkErr(err)
+		counter = counter + 1
+	}
+}
+
+func yoinkUUID(db *sql.DB) []string {
+	rows, err := db.Query("SELECT distinct policy_id from user_policy_object_condition;")
+	checkErr(err)
+	var res []string
+	for rows.Next() {
+		var policy_id string
+		err1 := rows.Scan(&policy_id)
+		checkErr(err1)
+		res = append(res, policy_id)
+	}
+	return res
+}
+
+func getID(db *sql.DB) []string {
+	var res []string
+	rows, err := db.Query("SELECT id from usertable;")
+	checkErr(err)
+	for rows.Next() {
+		var key string
+		err1 := rows.Scan(&key)
+		checkErr(err1)
+		res = append(res, key)
+	}
+	return res
+}
+
+func delOldmData(db *sql.DB) {
+	_, err := db.Query("DELETE FROM user_policy;")
+	checkErr(err)
+}
+
+func updateTomb(db *sql.DB) {
+	// _, err := db.Exec("ALTER TABLE usertable ADD COLUMN tomb integer;")
+	// checkErr(err)
+	_, err1 := db.Exec("UPDATE usertable set tomb = 0;")
+	checkErr(err1)
+}
+
+// TODO: SELECT distinct policy_id from user_policy_object_condition
+// Generate random metadata for each policy_id
+// INSERT into user_policy() VALUES()
+func main() {
+	db := setupDB()
+	// delOldmData(db)
+	// updateTomb(db)
+	listOfIds := getID(db)
+	fmt.Println(len(listOfIds))
+	refreshTTL(listOfIds, db)
+	// insertPolicy(listOfIds, db)
+	fmt.Println("DONE :)")
+
+}
+
+// DB set up
+func setupDB() *sql.DB {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+
+	checkErr(err)
+
+	return db
+}
+
+// Function for handling errors
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
